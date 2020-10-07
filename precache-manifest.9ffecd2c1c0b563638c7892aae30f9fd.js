5bcfad4665bb1e1a0918c99254212ee3d6a919dd
shubha0008 <shubharashmi101@gmail.com>
2020-10-07 13:38:05 +0530

    Updates

---
+++ precache-manifest.9ffecd2c1c0b563638c7892aae30f9fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44e90c2b4588325838f25c999092e80",
+    "url": "/broadcast/index.html"
+  },
+  {
+    "revision": "87ff163670df30cb261e",
+    "url": "/broadcast/static/css/2.29c18047.chunk.css"
+  },
+  {
+    "revision": "edad4b55a1b63ba333e9",
+    "url": "/broadcast/static/css/main.783d2928.chunk.css"
+  },
+  {
+    "revision": "87ff163670df30cb261e",
+    "url": "/broadcast/static/js/2.5fbee6f4.chunk.js"
+  },
+  {
+    "revision": "df3b9fa111835b5160daf3dc2b14fe52",
+    "url": "/broadcast/static/js/2.5fbee6f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edad4b55a1b63ba333e9",
+    "url": "/broadcast/static/js/main.c77601a5.chunk.js"
+  },
+  {
+    "revision": "f870176686e847d8e53e",
+    "url": "/broadcast/static/js/runtime-main.de6bbff7.js"
+  }
+]);
